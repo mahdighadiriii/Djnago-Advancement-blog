b3cd4a333808b12dc9c062ebbfaaca5650e58381
mahdighadiriii <mahdighadiriafzal@gmail.com>
2023-09-08 19:52:44 +0330

    reformat

--- core/blog/management/commands/insert_data.py
+++ core/blog/management/commands/insert_data.py
@@ -8,10 +8,6 @@ from accounts.models import User, Profile
 from blog.models import Post, Category
 
 
-
-
-
-
 category_list = ["IT", "Design", "Fun"]
 
 
@@ -23,7 +19,9 @@ class Command(BaseCommand):
         self.fake = Faker()
 
     def handle(self, *args, **options):
-        user = User.objects.create_user(email=self.fake.email(), password="Test@123456")
+        user = User.objects.create_user(
+            email=self.fake.email(), password="Test@123456"
+        )
         profile = Profile.objects.get(user=user)
         profile.first_name = self.fake.first_name()
         profile.last_name = self.fake.last_name()
@@ -39,6 +37,8 @@ class Command(BaseCommand):
                 title=self.fake.paragraph(nb_sentences=1),
                 content=self.fake.paragraph(nb_sentences=10),
                 status=random.choice([True, False]),
-                category=Category.objects.get(name=random.choice(category_list)),
+                category=Category.objects.get(
+                    name=random.choice(category_list)
+                ),
                 published_date=datetime.now(),
             )
