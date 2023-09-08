5e77b24988c856718efcb88a6c9acaaabe40f8c3
mahdighadiriii <mahdighadiriafzal@gmail.com>
2023-09-08 21:08:00 +0330

    laod testing

---
+++ core/locust/locustfile.py
@@ -0,0 +1,18 @@
+from locust import HttpUser, task, between
+
+class QuickstartUser(HttpUser):
+    
+    def on_start(self):
+        response = self.client.post('/accounts/api/v2/jwt/create', data={
+            "email": "admin@admin.com",
+            "password": "123"
+        }).json()
+        self.client.headers = {'Authorization': f"Bearer {response.get('access',None)}"}
+
+    @task
+    def post_list(self):
+        self.client.get("/blog/api/v1/post/")
+
+    @task
+    def category_list(self):
+        self.client.get("/blog/api/v1/category/")
